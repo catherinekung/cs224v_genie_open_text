4e2f46a12d02e42d9e9b08df8ce05826475c3204
angiet642 <96565058+angiet642@users.noreply.github.com>
2023-11-03 00:08:07 -0700

    i fixed the file and data_base creation in the worst way possible

--- pipelines/chatbot.py
+++ pipelines/chatbot.py
@@ -16,7 +16,7 @@ from .split_claim import ClaimSplitter
 from .utils import is_everything_verified, extract_year, extract_vote
 from .final_project.reviews_util import (extract_restaurant_topic, fetch_reviews, extract_relevant_content,
                                          summarize_reviews, extract_topics_from_review)
-
+from .final_project.read_mongo_data import Yelp_Data
 logger = logging.getLogger(__name__)
 
 if not logger.handlers:
@@ -36,6 +36,9 @@ class Chatbot:
             self.claim_splitter = ClaimSplitter(args)
             self.evi_num = args.evi_num
         self.colbert_endpoint = args.colbert_endpoint
+        self.yelp_review = Yelp_Data()
+        self.data_file = r'pipelines/final_project/dump_data/yelp_data.bson'
+        self.data_base = self.yelp_review.get_database(self.data_file)
         self.restaurant = None
     def generate_next_turn(
             self,
@@ -555,9 +558,9 @@ class Chatbot:
         topics = ["ambiance"]#, "food quality", "service", "price"] # hardcoded for now
         # if first turn:
         #     self.restaurant
-        topics_user_spec, restaurant = extract_restaurant_topic(new_user_utterance)
+        topics_user_spec, restaurant = self.yelp_review.get_topic_and_restaurant(new_user_utterance)
         # topics = [topics_user_spec]
-        reviews = fetch_reviews(new_user_utterance)
+        reviews = self.yelp_review.fetch_reviews(new_user_utterance, self.data_base)
         # reviews = ["My friend and I arrived here on a Friday night around 7:30 PM, and it wasn't busy at all. Lots of free space and some customers flowing in for mostly to go orders. One of the few places that open until late.   PARKING: It's part of a huge plaza, so you don't have to worry about parking.   AMBIENCE: The atmosphere of the place is chill and casual. Lots of seating from high to low tables. Great indoor lighting, nice seating, and love the self serve sauce station for unlimited honey mustard and ketchup.   SERVICE: Super fast service, no wait in the line. The cashier delivers you the food to your table and confirms your order.   ORDERS: I ordered the chicken gyro pita, and I love how it's packed with protein and veggies. They got my request right since I didn't want cucumbers in my gyro. Other than it being drenched with sauce, it was a good meal. Chicken on spinning rotisserie is so flavorful, especially with fresh tomatoes, lettuce, and red onions. The pita was so soft to munch on, chicken was tender, and the veggies had a great crunch factor.   RATING: Good place for Greek food!",
         #            "The foods are big portions and super yummy. We ordered  no 4 Kao Gai Sap and no 7 penang curry chicken. Taste super duper good, the price are very affordable. The owner are very nice, and the place is neat, clean and great ambiance. We will return here and high recommended!"]
 
--- pipelines/final_project/read_mongo_data.py
+++ pipelines/final_project/read_mongo_data.py
@@ -1,25 +1,23 @@
 import bson
 import re
 class Yelp_Data():
-
-
     # just return data_reviews_only
 
-    def __init__(self):
+    # def __init__(self):
+    def get_database(self, file_path):
         file = r'pipelines/final_project/dump_data/yelp_data.bson'
-
-        with open(r'pipelines/final_project/dump_data/yelp_data.bson', 'rb') as f:
-
-        # with open(r'dump_data/yelp_data.bson', 'rb') as f:
+        # with open(r'pipelines/final_project/dump_data/yelp_data.bson', 'rb') as f:
+        with open(file_path, 'rb') as f:
             data = bson.decode_all(f.read())
-        self.data_reviews_only = {}
+        data_reviews_only = {}
         for i in range(len(data)):
             # Check that location, name, and review exist
             if ("name" in data[i]) and ("reviews" in data[i]) and ("location" in data[i]):
                 location = (data[i]["location"]["city"], data[i]["location"]["address1"], data[i]["location"]["zip_code"])
                 name = data[i]["name"] # restaurant name
                 reviews = data[i]["reviews"] # all reviews
-                self.add_element(self.data_reviews_only, name, location, reviews)
+                self.add_element(data_reviews_only, name, location, reviews)
+        return data_reviews_only
 
     # give restuarants with rating 3.0
     #{name: {location: reviews}}
@@ -56,16 +54,16 @@ class Yelp_Data():
             raise Exception("Check input, topic or restaurant not extracted")
         return topic, restaurant, zip
 
-    def fetch_reviews(self, user_input):
+    def fetch_reviews(self, user_input, data_reviews_only):
         topic, restaurant = self.get_topic_and_restaurant(user_input)
-        num_locations = len(self.data_reviews_only[restaurant])
+        num_locations = len(data_reviews_only[restaurant])
         location_idx = ""
         location_key = ""
         if num_locations >= 1:
             topic, restaurant, zip = self.get_topic_and_restaurant_with_location(user_input)
 
             # Get last location review
-            all_reviews = self.data_reviews_only[restaurant]
+            all_reviews = data_reviews_only[restaurant]
             for i in range(num_locations):
                 orig_keys = all_reviews[i].keys()
                 keys = list(orig_keys)
@@ -78,7 +76,7 @@ class Yelp_Data():
                     location_key = keys[0]
             reviews = all_reviews[location_idx][location_key]
         else:
-            reviews = self.data_reviews_only[restaurant]
+            reviews = data_reviews_only[restaurant]
         print("\tReview from:", location_key)
         return reviews
         # print("\tReviews: ", reviews)
@@ -86,7 +84,9 @@ class Yelp_Data():
 if __name__ == "__main__":
     print("Hello, World!")
     yelp_reviews = Yelp_Data()
+    file_path = r'dump_data/yelp_data.bson'
+    data_base = yelp_reviews.get_database(file_path)
     user_input = "Tell me about the ambiance at The Funny Farm"
-    reviews = yelp_reviews.fetch_reviews(user_input)
+    reviews = yelp_reviews.fetch_reviews(user_input, data_base)
     for i in reviews:
         print(i)
